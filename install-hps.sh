b3021bf849355fe725675b32e5d1d295cafbe43e
AkiharaHoshina <chenmu346@gmail.com>
2026-01-29 12:42:14 +0000

    111

--- install-hps.sh
+++ install-hps.sh
@@ -3,6 +3,15 @@ echo "已经创造目录"
 wget -O ~/.hps/upgrade-sunset.sh https://raw.githubusercontent.com/AkiharaHoshina/HoshinaPush/refs/heads/main/hps/upgrade-sunset.sh || exit 1
 wget -O ~/.hps/version.sh https://raw.githubusercontent.com/AkiharaHoshina/HoshinaPush/refs/heads/main/hps/version.sh || exit 1
 echo "已经下崽脚本"
+pkg install openssh
+echo "已经安装openssh"
+pkg install fastfetch
+echo "已经安装fastfetch"
+pkg install python-pip
+echo "已经安装pip"
+pip install --upgrade hyfetch
+echo "已经安装hyfetch"
+echo "正在插入石山"
 echo "" >> ~/.bashrc
 echo "#Hoshina Push Services" >> ~/.bashrc
 echo "alias hps.version='sh ~/.hps/version.sh'" >> ~/.bashrc
